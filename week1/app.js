147741cdda0b6643b6b86c01902cb7cc86d8bb63
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-03-23 20:42:57 +0100

    week1 hw is done

---
+++ week1/app.js
@@ -0,0 +1,157 @@
+
+const API = {
+
+    endpoints: {
+
+        repos: function (page = 100) {
+            return 'https://api.github.com/orgs/HackYourFuture/repos?per_page=' + page;
+        }
+
+    }
+
+};
+
+function fetchJSON(url, callback) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+
+            if (typeof callback === 'function') {
+
+                if (xhr.status < 400) {
+
+                    callback(null, xhr.response);
+
+                } else {
+
+                    callback(new Error(xhr.statusText));
+                }
+
+            }
+
+        }
+    };
+    xhr.send();
+}
+
+function getRepos() {
+
+    fetchJSON(API.endpoints.repos(), (err, response) => {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const repos = response.map((repo) => {
+            return {
+                name: repo.name,
+                description: repo.description,
+                forks: repo.forks,
+                updated_at: repo.updated_at,
+                contributors_url: repo.contributors_url
+            };
+        });
+
+        renderSelect(repos);
+
+    });
+
+}
+
+function append(tagName, parent) {
+    const elem = document.createElement(tagName);
+    parent.appendChild(elem);
+    return elem;
+}
+
+function renderSelect(repositories) {
+
+    const root = document.getElementById('root');
+
+    const header = append('header', root);
+
+    const title = append('span', header);
+    title.innerHTML = "HYF Repositories";
+
+    const select = append('select', header);
+
+    repositories.forEach(repo => {
+        const option = append('option', select);
+        option.setAttribute('value', JSON.stringify(repo));
+        option.innerHTML = repo.name;
+    });
+
+    select.addEventListener('change', onSelectChanged);
+
+    const main = append('main', root);
+    main.innerHTML = "<div id='left-sidebar'></div><div id='right-sidebar'></div>";
+
+}
+
+function onSelectChanged() {
+
+    const repo = JSON.parse(event.target.value);
+
+    const repoDetailHtml = "<table>"
+        + "<tr>"
+        + "<td>Repository:</td>"
+        + "<td>" + repo.name + "</td>"
+        + "</tr>"
+        + "<tr>"
+        + "<td>Description:</td>"
+        + "<td>" + (repo.description == null ? "" : repo.description) + "</td>"
+        + "</tr>"
+        + "<tr>"
+        + "<td>Forks:</td>"
+        + "<td>" + repo.forks + "</td>"
+        + "</tr>"
+        + "<tr>"
+        + "<td>Updated:</td>"
+        + "<td>" + repo.updated_at + "</td>"
+        + "</tr>"
+        + "</table>";
+
+    const leftSidebar = document.getElementById("left-sidebar");
+    leftSidebar.innerHTML = repoDetailHtml;
+
+    const rightSidebar = document.getElementById("right-sidebar");
+    rightSidebar.innerHTML = "Loading...";
+
+    fetchJSON(repo.contributors_url, (err, response) => {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const contributors = response.map((contributor) => {
+            return {
+                name: contributor.login,
+                avatar: contributor.avatar_url,
+                contributions: contributor.contributions
+            };
+        });
+
+        var contributorsHtml = "<table>";
+
+        contributors.forEach(contributor => {
+            contributorsHtml += "<tr>"
+                + "<td><img src='" + contributor.avatar + "'/></td>"
+                + "<td>" + contributor.name + "</td>"
+                + "<td>" + contributor.contributions + "</td>"
+                + "</tr>";
+        });
+
+        contributorsHtml += "</table>";
+
+        rightSidebar.innerHTML = contributorsHtml;
+
+    })
+}
+
+
+getRepos();
