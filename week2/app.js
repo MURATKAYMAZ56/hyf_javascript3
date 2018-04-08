3be46dbb644235a105bdcca9945a40c078e5695a
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-04-08 12:17:42 +0200

    gixed entriely

--- week2/app.js
+++ week2/app.js
@@ -1,78 +1,51 @@
+'use strict';
 
 const hyfUrl = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-
 
 function fetchJSON(method, url) {
     return new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
-
         xhr.open(method, url);
         xhr.responseType = 'json';
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
-
                 if (xhr.status < 400) {
-
                     resolve(xhr.response);
-
                 } else {
-
                     reject(new Error(xhr.statusText));
                 }
-
-
             }
         };
-
         xhr.send();
-
     });
-
-
 }
 
 function main() {
-
-    fetchJSON('GET', hyfUrl).then(
-        const repositories = response.map((repo) => {
-        return {
-            name: repo.name,
-            description: repo.description,
-            forks: repo.forks,
-            updated_at: repo.updated_at,
-            contributors_url: repo.contributors_url
-        };
-    })).catch (
-    err => {
-        console.log(err.message);
-    }
-        )
-
-
-
-
-
-
     const root = document.getElementById('root');
-
     const header = append('header', root);
-
     const title = append('span', header);
     title.innerHTML = "HYF Repositories";
-
     const select = append('select', header);
 
-    repositories.forEach(repo => {
-        const option = append('option', select);
-        option.setAttribute('value', JSON.stringify(repo));
-        option.innerHTML = repo.name;
-    });
+    fetchJSON('GET', hyfUrl)
+        .then(repositories => {
 
-    select.addEventListener('change', onSelectChanged);
+            repositories.forEach((repo, index) => {
+                const option = append('option', select);
+                option.setAttribute('value', index);
+                option.innerHTML = repo.name;
+            });
 
-    const main = append('main', root);
-    main.innerHTML = "<div id='left-sidebar'></div><div id='right-sidebar'></div>";
+            select.addEventListener('change', () => onSelectChanged(repositories[select.value]));
 
+            const main = append('main', root);
+            main.innerHTML = "<div id='left-sidebar'></div><div id='right-sidebar'></div>";
+
+            onSelectChanged(repositories[0]);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 
@@ -82,9 +55,7 @@ function append(tagName, parent) {
     return elem;
 }
 
-function onSelectChanged(event) {
-
-    const repo = JSON.parse(event.target.value);
+function onSelectChanged(repo) {
 
     const repoDetailHtml = "<table>"
         + "<tr>"
@@ -111,40 +82,22 @@ function onSelectChanged(event) {
     const rightSidebar = document.getElementById("right-sidebar");
     rightSidebar.innerHTML = "Loading...";
 
-    fetchJSON(repo.contributors_url).then(
-        const contributors = response.map((contributor) => {
-        return {
-            name: contributor.login,
-            avatar: contributor.avatar_url,
-            contributions: contributor.contributions
-        };
-    });
-
-    let contributorsHtml = "<table>";
-
-    contributors.forEach(contributor => {
-        contributorsHtml += "<tr>"
-            + "<td><img src='" + contributor.avatar + "'/></td>"
-            + "<td>" + contributor.name + "</td>"
-            + "<td>" + contributor.contributions + "</td>"
-            + "</tr>";
-    });
-
-    contributorsHtml += "</table>";
-
-    rightSidebar.innerHTML = contributorsHtml;
-
-    ).catch ((err) => {
-
-        if (err) {
+    fetchJSON('GET', repo.contributors_url)
+        .then(contributors => {
+            let contributorsHtml = "<table>";
+            contributors.forEach(contributor => {
+                contributorsHtml += "<tr>"
+                    + "<td><img src='" + contributor.avatar_url + "'/></td>"
+                    + "<td>" + contributor.login + "</td>"
+                    + "<td>" + contributor.contributions + "</td>"
+                    + "</tr>";
+            });
+            contributorsHtml += "</table>";
+            rightSidebar.innerHTML = contributorsHtml;
+        })
+        .catch((err) => {
             console.log(err);
-            return;
-        }
-
-
-    });
-
+        });
 }
 
-
-main();
+window.onload = main;
